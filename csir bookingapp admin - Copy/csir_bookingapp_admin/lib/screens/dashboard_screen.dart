d9e14223538739dbffd93662fc40b52a970a4c41
eric nana takyi <ericnanatakyi@gmail.com>
2023-02-21 05:32:12 -0800

    commit 1

---
+++ csir bookingapp admin - Copy/csir_bookingapp_admin/lib/screens/dashboard_screen.dart
@@ -0,0 +1,46 @@
+import 'package:csir_bookingapp_admin/consts/constants.dart';
+import 'package:csir_bookingapp_admin/widgets/header.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../controllers/MenuController.dart' as menucontroller;
+
+class DashboardScreen extends StatelessWidget {
+  const DashboardScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SingleChildScrollView(
+        padding: const EdgeInsets.all(defaultPadding),
+        child: Column(
+          children: [
+            Header(
+              fct: () {
+                context
+                    .read<menucontroller.MenuController>()
+                    .controlDashboarkMenu();
+              },
+            ),
+            const SizedBox(height: defaultPadding),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  flex: 5,
+                  child: Column(
+                    children: const [
+                      // MyProductsHome(),
+                      // SizedBox(height: defaultPadding),
+                      // OrdersScreen(),
+                    ],
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
